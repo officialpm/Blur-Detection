849a5b7cf1f7a2f80144e1dfda2f0621e3dd9206
Parth Maniar <officialparthmaniar@gmail.com>
2020-06-12 12:31:10 +0530

    :tada: Changes

--- app.py
+++ app.py
@@ -18,13 +18,15 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 fm = variance_of_laplacian(gray)
 blur = False
 text = "Not Blurry"
+print("Not Blurry")
 # if the focus measure is less than the supplied threshold,
 # then the image should be considered "blurry"
 if fm < threshold:
-    blur = True
-    text = "Blurry"
+	print("Blur")
+	blur = True
+	text = "Blurry"
 # show the image
 cv2.putText(image, "{}: {:.2f}".format(text, fm), (20, 30),
     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (140, 0, 255), 2)
 cv2.imshow("Image", image)
-key = cv2.waitKey(0)
+key = cv2.waitKey()
